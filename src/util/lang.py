41934696917f692cda17672b923a1ff996196fa0
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2021-03-08 21:00:55 +0100

    Now changing app language should change Mycroft's too #7

--- src/util/lang.py
+++ src/util/lang.py
@@ -1,16 +1,18 @@
 import gettext
 from babel import Locale
-from typing import Union, List
+from typing import Union, List, Tuple
 import os
+import json
 
 
 class Translator:
     def __init__(self, lang: str) -> None:
-        self._lang = None
+        self._lang = None # String like en_US
         self._available_langs = []
         self._language_names = []
         self._domain = "UBUVoiceAssistant"
         self._lang_dir = "./lang"
+        self._translator: gettext.GNUTranslations = None
         self.find_available_languages()
         self.change_language(lang)
 
@@ -18,12 +20,12 @@ class Translator:
         if isinstance(lang, int):
             self.change_language(self._available_langs[lang])
         self._lang = lang
-        translator = gettext.translation(
+        self._translator = gettext.translation(
             self._domain, self._lang_dir, [self._lang])
-        translator.install()
+        self.update_mycroft_config()
 
     def translate(self, string: str) -> str:
-        return _(string)
+        return self._translator.gettext(string)
 
     def find_available_languages(self) -> List[str]:
         self._available_langs = []
@@ -34,3 +36,22 @@ class Translator:
                 self._language_names.append(
                     Locale(*locale.split("_")).language_name)
         return self._language_names
+
+    def get_current_language(self) -> Tuple[str]:
+        current_name = self._language_names[self._available_langs.index(self._lang)]
+        return self._lang, current_name
+
+    def update_mycroft_config(self) -> None:
+        lang_string = self.get_current_language()[0].lower().replace("_", "-")
+        with open("~/.config/mycroft-docker/mycroft.conf", "r") as mycroft_cfg_file:
+            mycroft_cfg = json.load(mycroft_cfg_file)
+        mycroft_cfg["lang"] = lang_string
+        if "tts" not in mycroft_cfg:
+            mycroft_cfg["tts"] = {}
+        mycroft_cfg["tts"]["module"] = "google"
+        if "google" not in mycroft_cfg["tts"]:
+            mycroft_cfg["tts"]["google"] = {}
+        mycroft_cfg["tts"]["google"]["lang"] = lang_string
+        mycroft_cfg["tts"]["google"]["slow"] = False
+        with open("~/.config/mycroft-docker/mycroft.conf", "w") as mycroft_cfg_file:
+            json.dump(mycroft_cfg, mycroft_cfg_file)
