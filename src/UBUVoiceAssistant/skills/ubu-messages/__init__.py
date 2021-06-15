f5071119bb125cf7a1f0d748695220d3708bde6f
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2021-06-15 14:11:28 +0200

    Updated read messages and first steps on sending

--- src/UBUVoiceAssistant/skills/ubu-messages/__init__.py
+++ src/UBUVoiceAssistant/skills/ubu-messages/__init__.py
@@ -4,6 +4,7 @@ from mycroft import MycroftSkill, intent_handler # type: ignore
 from mycroft.audio import wait_while_speaking
 sys.path.append("/usr/lib/UBUVoiceAssistant") # type: ignore
 from UBUVoiceAssistant.util import util # type: ignore
+from UBUVoiceAssistant.model.conversation import Conversation
 
 
 class UbuMessagesSkill(MycroftSkill):
@@ -16,17 +17,26 @@ class UbuMessagesSkill(MycroftSkill):
 
     @intent_handler(IntentBuilder("UnreadMessagesIntent").require("UnreadMessagesVoc"))
     def handle_unread_messages(self, message):
+        self.speak("Dame un momento, por favor")
         convers = self.ws.get_conversations_with_messages()
         messages = {}
+        msg_from = {}
         for conver in convers:
             messages.update(conver.get_messages())
+            for m in conver.get_messages().values():
+                msg_from[m.get_message_id()] = list(conver.get_members().values())[0].get_fullname()
         l = messages.keys()
         l = sorted(l, reverse=True)
         for n, m in enumerate(l):
-            self.speak(messages[m].get_clean_text())
+            self.speak(msg_from[m] + " dice " + messages[m].get_clean_text())
             wait_while_speaking()
             if n == 4:
                 break
+
+    @intent_handler(IntentBuilder("SendMessage").require("EnviarAPersona"))
+    def send_message(self, message):
+        persona = message.data.get("EnviarAPersona")
+        self.speak(persona)
 
 def create_skill():
     return UbuMessagesSkill()
