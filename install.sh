937a3024adc32b78fd88a5c99a31cf64ab6d19d2
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2021-06-24 13:55:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,6 +69,7 @@ install() {
   echo "cd /usr/lib/UBUVoiceAssistant" >> /usr/bin/UBUVoiceAssistant
   echo "QT_QUICK_BACKEND=\"software\" python3 -m UBUVoiceAssistant.GUI.main" >> /usr/bin/UBUVoiceAssistant
   chmod a+x /usr/bin/UBUVoiceAssistant
+  chown $USERNAME /usr/lib/UBUVoiceAssistant
   sudo -u $USERNAME cp -r ./src/UBUVoiceAssistant/ /usr/lib/UBUVoiceAssistant
   printf "${GREEN}Installed to a permanent location${NC}\n"
 
